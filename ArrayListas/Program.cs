6fdb1c61b17862f7fc678f719547a6588c2b1425
Douglas Bueno <douglas.bbueno16@gmail.com>
2025-08-17 18:41:58 -0300

    add: exemplo de array e listas

---
+++ ArrayListas/Program.cs
@@ -0,0 +1,13 @@
+﻿// Exemplos de ArrayListas
+
+int[] arrayInteiros = new int[3];
+
+arrayInteiros[0] = 10;
+arrayInteiros[1] = 20; 
+arrayInteiros[2] = 30;
+
+for (int contador = 0; contador < arrayInteiros.Length; contador++)
+{ 
+    Console.WriteLine($"Valor na posição {contador} é {arrayInteiros[contador]}");
+
+}  // for

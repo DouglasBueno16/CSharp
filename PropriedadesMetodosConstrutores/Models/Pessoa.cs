4afb57ff1800f5801b58507b12bd2ee5afdcade8
Douglas Bueno <douglas.bbueno16@gmail.com>
2025-08-21 19:09:27 -0300

    add: classe Pessoa e seus métodos

---
+++ PropriedadesMetodosConstrutores/Models/Pessoa.cs
@@ -0,0 +1,44 @@
+﻿namespace PropriedadesMetodosConstrutores;
+
+public class Pessoa
+{
+    private string _nome;  // campo privado para armazenar o nome
+    public string Nome
+    {
+        get => _nome.ToUpper();  // retorna o nome em maiúsculas              
+        set
+        {
+            if (value == "")
+            {
+                throw new ArgumentException("O nome não pode ser vazio.");
+            }
+            _nome = value;
+        }
+    }
+
+    public string Sobrenome { get; set; }
+
+    public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();  // retorna a concatenação das classes 
+
+    private int _idade;  // campo privado para Idade
+    public int Idade
+    {
+        get => _idade;  // retorna a idade
+
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("A idade não pode ser negativa.");
+            }
+            _idade = value;
+        }
+        
+    }
+
+    public void Apresentar()
+    {
+        Console.WriteLine($"Olá, meu nome é {NomeCompleto} e tenho {Idade} anos.");
+    }  // Apresentar
+
+}  // class Pessoa
---
+++ PropriedadesMetodosConstrutores/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Propriedades e métodos construtores
+*/
+
+using PropriedadesMetodosConstrutores;
+
+Pessoa p1 = new Pessoa
+{
+    Nome = "Douglas",
+    Sobrenome = "Bueno",
+    Idade = 32
+};
+p1.Apresentar();
+
+

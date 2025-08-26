dee002e9b52c897203ec8c90b8ab96a2393a79d8
Douglas Bueno <douglas.bbueno16@gmail.com>
2025-08-26 20:51:57 -0300

    add: classe curso

---
+++ PropriedadesMetodosConstrutores/Models/Curso.cs
@@ -0,0 +1,32 @@
+﻿namespace PropriedadesMetodosConstrutores;
+
+public class Curso
+{
+    public string Nome { get; set; }
+    public List<Pessoa> Alunos { get; set; }
+
+    public void AdicionarAluno(Pessoa aluno)
+    {
+        Alunos.Add(aluno);
+    }  // adicionar aluno
+
+    public int ObterQuantidadeDeAlunosMatriculados()
+    {
+        return Alunos.Count;
+    }
+
+    public void RemoverAluno(Pessoa aluno)
+    {
+        Alunos.Remove(aluno);
+    }
+
+    public void ListarAlunos()
+    {
+        foreach (Pessoa aluno in Alunos) {
+
+            Console.WriteLine(aluno.NomeCompleto);
+        }        
+    }
+
+
+}  // Curso
--- PropriedadesMetodosConstrutores/Program.cs
+++ PropriedadesMetodosConstrutores/Program.cs
@@ -2,14 +2,28 @@
 Propriedades e métodos construtores
 */
 
+using System.ComponentModel.Design.Serialization;
 using PropriedadesMetodosConstrutores;
 
 Pessoa p1 = new Pessoa
 {
     Nome = "Douglas",
     Sobrenome = "Bueno",
-    Idade = 32
+    Idade = 21
 };
-p1.Apresentar();
+// p1.Apresentar();
 
+Pessoa p2 = new Pessoa
+{
+    Nome = "João",
+    Sobrenome = "Silva",
+    Idade = 22
+};
 
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();

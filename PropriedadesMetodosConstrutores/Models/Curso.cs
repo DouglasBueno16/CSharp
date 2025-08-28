5a6f285049949e1e3e9a72ededaa6d6a5335eb81
Douglas Bueno <douglas.bbueno16@gmail.com>
2025-08-28 19:14:27 -0300

    refactor: update Curso initialization and improve ListarAlunos method

--- PropriedadesMetodosConstrutores/Models/Curso.cs
+++ PropriedadesMetodosConstrutores/Models/Curso.cs
@@ -5,6 +5,11 @@ public class Curso
     public string Nome { get; set; }
     public List<Pessoa> Alunos { get; set; }
 
+    public Curso(string nome)
+    {
+        Nome = nome;
+        Alunos = new List<Pessoa>();
+    }
     public void AdicionarAluno(Pessoa aluno)
     {
         Alunos.Add(aluno);
@@ -22,11 +27,14 @@ public class Curso
 
     public void ListarAlunos()
     {
-        foreach (Pessoa aluno in Alunos) {
+        Console.WriteLine($"Alunos matriculados no curso {Nome}: ");
 
-            Console.WriteLine(aluno.NomeCompleto);
-        }        
-    }
-
+        for (int count = 0; Alunos.Count > count; count++ )
+        {
+            string texto = $"Nº {count+1} - {Alunos[count].NomeCompleto}";  // interpolação
+            Console.WriteLine(texto);
+        }
+       
+    }  // listar alunos
 
 }  // Curso
--- PropriedadesMetodosConstrutores/Program.cs
+++ PropriedadesMetodosConstrutores/Program.cs
@@ -5,23 +5,10 @@ Propriedades e métodos construtores
 using PropriedadesMetodosConstrutores;
 
 Pessoa p1 = new Pessoa(nome: "Douglas", sobrenome: "Bueno");
-// {
-//     Nome = "Douglas",
-//     Sobrenome = "Bueno",
-//     Idade = 21
-// };
-// p1.Apresentar();
 
 Pessoa p2 = new Pessoa(nome: "João", sobrenome: "Silva");
-// {
-//     Nome = "João",
-//     Sobrenome = "Silva",
-//     Idade = 22
-// };
 
-Curso cursoDeIngles = new Curso();
-cursoDeIngles.Nome = "Inglês";
-cursoDeIngles.Alunos = new List<Pessoa>();
+Curso cursoDeIngles = new Curso(nome: "Inglês");
 
 cursoDeIngles.AdicionarAluno(p1);
 cursoDeIngles.AdicionarAluno(p2);
